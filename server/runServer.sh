9dc353f1768a7f6da454af48205aadcb8f1f7bbe
Motasem-Diab <58920309+Motasem-Diab@users.noreply.github.com>
2020-08-24 10:59:23 +0300

    Update runServer.sh

--- server/runServer.sh
+++ server/runServer.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
-
-IDs=`docker ps | grep "server-image" | awk '{ print $1 }'`  #this will return running "server-image" containers IDs in order to stop it
+#return running "server-image" containers IDs in order to stop it
+IDs=`docker ps | grep "server-image" | awk '{ print $1 }'`  
 if [ $IDs ]
 then
-	docker stop $IDs						#stop all running images
+	docker stop $IDs	#stop all running images
 	echo "*************** stopping the previous image ************"
 fi
 
-IDs=`docker ps -a | grep "server-image" | awk '{ print $1 }'`	#this will return all stoped "server-image" containers IDs in order to remove it
+#return all stoped "server-image" containers IDs in order to remove it
+IDs=`docker ps -a | grep "server-image" | awk '{ print $1 }'`	
 if [ $IDs ]
 then
         docker rm $IDs
         echo "*************** removing the previous images ************"
 fi
 
+#build the server-image
+docker build . -t server-image		
 
-docker build . -t server-image		#this command will build the server-image
-
-docker run -it server-image 		#this command will run the server-image
+#run a container of the server-image
+docker run -it server-image 		
