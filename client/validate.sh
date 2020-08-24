599a68a7f8afc60a50f8cb6c4e126ed01d87560e
Motasem-Diab <58920309+Motasem-Diab@users.noreply.github.com>
2020-08-24 10:56:43 +0300

    Update validate.sh

--- client/validate.sh
+++ client/validate.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-answer=$(yum list installed  | grep httpd | awk '{ print $1 }')		#this will grep lines contains "httpd" then get the name
+#grep lines contains "httpd" then get the name
+answer=$(yum list installed  | grep httpd | awk '{ print $1 }')		
 
-if [[ "$answer" == *httpd\.x86* ]]					#if the answer has httpd.x86 
+if [[ "$answer" == *httpd\.x86* ]]    #if the answer has httpd.x86 
 then
         echo "*******************               *******************"
         echo "               httpd installed                "
         echo "*******************               *******************"
-else
+else    # it's not containing "httpd.x86"
         echo "*******************               *******************"
         echo "           httpd is not installed                    "
         echo "*******************               *******************"
